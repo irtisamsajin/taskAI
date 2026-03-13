f95933cdf7e64bbd1b68373531627ab3cce70002
irtisamsajin <irtisam.coding@gmail.com>
2026-03-13 22:38:01 +0600

    Setup

--- server/database/database.py
+++ server/database/database.py
@@ -6,7 +6,7 @@ DATABASE_URL=os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
-SessionLocal=sessionmaker(bind=engine)
+SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
 Base=declarative_base()
 
